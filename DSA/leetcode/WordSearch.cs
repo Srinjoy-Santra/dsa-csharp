ab8694044fff14bfd4a8a65264eddd2a7a59deef
Srinjoy Santra <srinjoy_S@yahoo.com>
2024-12-17 09:02:45 +0530

    combining all if condition passes leetcode TLE lol!

--- DSA/leetcode/WordSearch.cs
+++ DSA/leetcode/WordSearch.cs
@@ -4,48 +4,36 @@ namespace DSA.leetcode;
 
 public class WordSearch
 {
+    private int _rows, _cols;
+    private HashSet<(int, int)> _path = new();
     public bool Exist(char[][] board, string word) {
-        int k=0;
-        int m = board.Length;
-        int n = board[0].Length;
-        BitArray visit = new(m * n);
-        for(int i=0;i<m;i++)
-        for(int j=0;j<n;j++)
+        _rows = board.Length;
+        _cols = board[0].Length;
+        for(int i=0;i<_rows;i++)
+        for(int j=0;j<_cols;j++)
         {
-            if(board[i][j] == word[k])
-            {
-                if(BackTrack(board, i,j, word, visit, k))
-                    return true;
-            }
+            if(BackTrack(board, i,j, word, 0))
+                return true;
         }
         return false;
     }
 
-    private  bool BackTrack(char[][]board, int i, int j, string word, BitArray visit, int k)
+    private  bool BackTrack(char[][]board, int i, int j, string word, int k)
     {
         if(k == word.Length)
             return true;
 
-        int m = board.Length;
-        int n = board[0].Length;
-        if(i<0||i>=m || j<0||j>=n)
-            return false;
-        Console.WriteLine(i+","+ j);
-
-        if(word[k] != board[i][j])
-            return false;
-        
-        if(visit.Get(i*n+j))
+        if(i<0||i>=_rows || j<0||j>=_cols || word[k] != board[i][j] || _path.Contains((i,j)))
             return false;
 
-        visit.Set(i*n+j, true);
+        _path.Add((i,j));
         
         // only vertical and horizontal search
-        bool exists = BackTrack(board, i-1, j, word, visit, k+1) 
-                      || BackTrack(board, i, j-1, word, visit, k+1) || BackTrack(board, i, j+1, word, visit, k+1) 
-                      || BackTrack(board, i+1, j, word, visit, k+1);
+        bool exists = BackTrack(board, i-1, j, word, k+1) 
+                      || BackTrack(board, i, j-1, word, k+1) || BackTrack(board, i, j+1, word, k+1) 
+                      || BackTrack(board, i+1, j, word, k+1);
 
-        visit.Set(i*n+j, false);
+        _path.Remove((i,j));
         return exists;
     }
 
